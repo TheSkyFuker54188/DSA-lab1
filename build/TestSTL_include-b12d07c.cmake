ec0fcc0eb290104f584c79055d275e86d9a55961
TheSkyFuker54188 <161278359+TheSkyFuker54188@users.noreply.github.com>
2024-11-25 14:31:43 +0800

    d

---
+++ build/TestSTL_include-b12d07c.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "D:/PROGRAMMING/_DSA/Lab1/build/TestSTL_tests-b12d07c.cmake")
+  include("D:/PROGRAMMING/_DSA/Lab1/build/TestSTL_tests-b12d07c.cmake")
+else()
+  add_test(TestSTL_NOT_BUILT-b12d07c TestSTL_NOT_BUILT-b12d07c)
+endif()
